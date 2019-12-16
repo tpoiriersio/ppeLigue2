24b24c8a42c7f890a512b927224b6e5b8ab7f8f2
Théo Poirier <tpoirier@S-204-16>
2019-12-16 11:19:04 +0100

    Création de Sql.cs, permettant de se connecter à la base et d'ajouter une séance via requête.

---
+++ PPE_ligue/Sql.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE_ligue
+{
+    public class Sql
+    {
+        private string IP = "172.19.0.3";
+        private string USER = "root";
+        private string PASSWORD = "0550002D";
+        private string DATABASE = "ligue";
+        private MySqlConnection connexion;
+
+        public Sql()
+        {
+            string connStr = "server=" + this.IP + ";user=" + this.USER + ";database=" + this.DATABASE + ";port=3306;password=" + this.PASSWORD;
+            connexion = new MySqlConnection(connStr);
+        }
+
+        public void insertSeance(string jour, string descriptif, DateTime debut, DateTime fin, string categorie)
+        {
+            string therequest = "INSERT INTO seance(`jour`,`descriptif`,`debut`,`fin`,`la_categorie`) VALUE ('" + jour + "','" + descriptif + "','" + debut + "','" + fin + "','" + categorie + "')";
+            MySqlCommand request = new MySqlCommand(therequest, this.connexion);
+            this.connexion.Open();
+            MySqlDataReader give = request.ExecuteReader();
+            this.connexion.Close();
+        }
+
+    }
+}
